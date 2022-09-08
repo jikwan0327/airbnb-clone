068483b2d7d2df8ab7b8471185f1ac1503511cd9
jikwan <jikwan0327@gmail.com>
2022-09-08 20:43:10 +0900

    edit fields

--- users/admin.py
+++ users/admin.py
@@ -5,9 +5,26 @@ from . import models
 # Register your models here.
 
 @admin.register(models.User)
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin ):
     
     """Cuestom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile", #파란색 칸의 제목
+            {
+                "fields": (
+                    "avatar" ,
+                    "gender",
+                    "bio", 
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            }
+        ),
+    )
 
     list_display = ('username', 'email', 'gender', 'language', 'currency', 'superhost')
     list_filter= ("language", "currency", 'superhost')
