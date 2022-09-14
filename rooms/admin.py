269c3c3b0a8ab50db78d5380a2a99b7edf93ebca
jikwan <jikwan0327@gmail.com>
2022-09-14 13:36:59 +0900

    Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,10 +13,73 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = ( #보이는 필드 구분
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")}
+        ),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths") }),
+        (
+            "More About the Space",
+            {
+                "classes": ('collapse',), #숨기기와 보이기 기능을 만들어줌
+                "fields": ("amenities", "facilities", "house_rules")
+            }
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        )
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities"
+    )
+
+    ordering = ('name', 'price', 'bedrooms')
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("^city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+    count_amenities.short_description = "hola"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
     """"""
-    pass
+    pass 
